54e8d1bc69305d1aafb8c1a5059ae883f22b3c1e
allen.chen <allen.chen@noaa.gov>
2018-12-13 13:37:48 -0800

    EPM functions checked to work

--- R/epm_weibull.r
+++ R/epm_weibull.r
@@ -6,11 +6,11 @@ epm_weibull <- function(starts3,dat,otherdat,alts) {
 #' @param starts3 Starting values
 #' @param dat Data matrix, see output from shiftSortX, alternatives with distance by column bind
 #' @param otherdat Other data used in model (as list). Any number of grid-varying variables (e.g. expected catch that varies by location) or 
-#' interaction variables (e.g. vessel characteristics that affect how much disutility is suffered by traveling a greater distance) are allowed. \cr
-#' However, the user must place these in `otherdat` as list objects named `griddat` and `intdat` respectively. \cr
-#' Note the variables #' within `griddat` and `intdat` have no naming restrictions. Also note that `griddat` variables are 
-#' dimension *(number of observations) x #' (number of alternatives)*, while `intdat` variables are dimension *(number of observations) x 1*, to be interacted 
-#' with the distance to each #' alternative. \cr
+#' interaction variables (e.g. vessel characteristics that affect how much disutility is suffered by traveling a greater distance) are allowed. \cr \cr
+#' However, the user must place these in `otherdat` as list objects named `griddat` and `intdat` respectively. Note the variables #' within `griddat` 
+#' and `intdat` have no naming restrictions. \cr \cr
+#' Also note that `griddat` variables are  dimension *(number of observations) x #' (number of alternatives)*, while `intdat` variables are 
+#' dimension *(number of observations) x 1*, to be interacted with the distance to each #' alternative. \cr \cr
 #' If there are no other data, the user can set `griddat` as ones with dimension *(number of observations) x (number of alternatives)*
 #' and `intdat` variables as ones with dimension *(number of observations) x 1*.
 #' @param alts Number of alternative choices in model
@@ -37,8 +37,8 @@ sigmac <- as.matrix(starts3[((length(griddat)*alts)+length(intdat)+2),]) #should
 for(i in 1:dim(dat)[1])
 {
 
-expgridcoef <- gridcoef*gamma(1 + (1/k))
-
+expgridcoef <- gridcoef*matrix(gamma(1 + (1/k)),dim(gridcoef)[1],1)
+ 
 betas1 <- c(t(as.matrix(do.call(rbind,lapply(griddat,`[`,i,)))*t(expgridcoef))%*%as.matrix(pricedat[i,]), 
 			t(as.matrix(do.call(rbind,lapply(intdat,`[`,i,))))%*%as.matrix(intcoef))
 betas <- t(as.matrix(betas1))
