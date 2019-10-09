72a8208ef467a05174ca53945efbdbe76812b8cc
allen.chen <allen.chen@noaa.gov>
2019-10-09 11:09:35 -0700

    First crack at graphical examples uses roxygen2

--- R/logit_c.r
+++ R/logit_c.r
@@ -6,36 +6,36 @@ logit_c <- function(starts3, dat, otherdat, alts) {
     #' @param starts3 Starting values as a vector (num). For this likelihood,
     #'     the order takes: c([alternative-specific parameters],
     #'     [travel-distance parameters]). \cr \cr
-	#'     The alternative-specific parameters and travel-distance parameters
-	#'     are of length (# of alternative-specific variables) and (# of
-	#'     travel-distance variables) respectively.
+    #'     The alternative-specific parameters and travel-distance parameters
+    #'     are of length (# of alternative-specific variables) and (# of
+    #'     travel-distance variables) respectively.
     #' @param dat Data matrix, see output from shift_sort_x, alternatives with
-	#'     distance.
+    #'     distance.
     #' @param otherdat Other data used in model (as a list containing objects
-	#'     `intdat` and `griddat`). \cr \cr
+    #'     `intdat` and `griddat`). \cr \cr
     #'     For this likelihood, `intdat` are "travel-distance variables", which
     #'     are alternative-invariant variables that are interacted with travel
     #'     distance to form the cost portion of the likelihood. Each variable
     #'     name therefore corresponds to data with dimensions (number of
     #'     observations) by (unity), and returns a single parameter. \cr \cr
-	#'     In `griddat` are "alternative-specific variables", that vary across
+    #'     In `griddat` are "alternative-specific variables", that vary across
     #'     alternatives, e.g. catch rates. Each variable name therefore
     #'     corresponds to data with dimensions (number of observations) by
-	#'     (number of alternatives), and returns a single parameter for each
-	#'     variable (e.g. the marginal utility from catch). \cr \cr
-	#'     For both objects any number of variables are allowed, as a list of
-	#'     matrices. Note the variables (each as a matrix) within `griddat` and
-	#'     `intdat` have no naming restrictions. "Alternative-specific
-	#'     variables" may correspond to catches that vary by location, and
-	#'     "travel-distance variables" may be vessel characteristics that affect
-	#'     how much disutility is suffered by traveling a greater distance. Note
-	#'     in this likelihood "alternative-specific variables" vary across
-	#'     alternatives because each variable may have been estimated in a
-	#'     previous procedure (i.e. a construction of expected catch). \cr \cr
-	#'     If there are no other data, the user can set `griddat` as ones with
-	#'     dimension (number of observations) by (number of alternatives) and
-	#'     `intdat` variables as ones with dimension (number of observations) by
-	#'     (unity).
+    #'     (number of alternatives), and returns a single parameter for each
+    #'     variable (e.g. the marginal utility from catch). \cr \cr
+    #'     For both objects any number of variables are allowed, as a list of
+    #'     matrices. Note the variables (each as a matrix) within `griddat` and
+    #'     `intdat` have no naming restrictions. "Alternative-specific
+    #'     variables" may correspond to catches that vary by location, and
+    #'     "travel-distance variables" may be vessel characteristics that affect
+    #'     how much disutility is suffered by traveling a greater distance. Note
+    #'     in this likelihood "alternative-specific variables" vary across
+    #'     alternatives because each variable may have been estimated in a
+    #'     previous procedure (i.e. a construction of expected catch). \cr \cr
+    #'     If there are no other data, the user can set `griddat` as ones with
+    #'     dimension (number of observations) by (number of alternatives) and
+    #'     `intdat` variables as ones with dimension (number of observations) by
+    #'     (unity).
     #' @param alts Number of alternative choices in model as length equal to
     #'     unity (as a numeric vector).
     #' @return ld: negative log likelihood
@@ -57,15 +57,24 @@ logit_c <- function(starts3, dat, otherdat, alts) {
     #' zi2 <- sample(1:10,dim(zi)[1],replace=TRUE)
     #'
     #' otherdat <- list(griddat=list(predicted_catch=as.matrix(predicted_catch),
-	#'     si2=as.matrix(si2)), intdat=list(zi=as.matrix(zi),
-	#'     zi2=as.matrix(zi2)))
+    #'     si2=as.matrix(si2)), intdat=list(zi=as.matrix(zi),
+    #'     zi2=as.matrix(zi2)))
     #'
     #' initparams <- c(2.5, 2, -1, -2)
     #'
     #' func <- logit_c
     #'
     #' results <- discretefish_subroutine(catch,choice,distance,otherdat,
-	#'     initparams,optimOpt,func,methodname)
+    #'     initparams,optimOpt,func,methodname)
+    #'
+    #' @section Graphical examples: 
+    #' \if{html}{
+    #' \figure{logit_c_grid.png}{options: width="50\%" 
+    #' alt="Figure: logit_c_grid.png"}
+    #' \cr
+    #' \figure{logit_c_travel.png}{options: width="50\%" 
+    #' alt="Figure: logit_c_travel.png"}
+    #' }
     #'
     
     griddat <- as.matrix(do.call(cbind, otherdat$griddat))
