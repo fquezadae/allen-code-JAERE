c4f789d9fdbe0240de0ca1b05f60245919860860
Felipe Quezada <fequezad@ucsc.edu>
2023-05-25 17:16:56 -0700

    update

--- R/discretefish_subroutine.r
+++ R/discretefish_subroutine.r
@@ -23,7 +23,7 @@ discretefish_subroutine <- function(catch, choice, distance, otherdat,
     #' @export
     #' @examples
     #'
-    
+
     errorExplain <- NULL
     OutLogit <- NULL
     optoutput <- NULL
@@ -32,78 +32,82 @@ discretefish_subroutine <- function(catch, choice, distance, otherdat,
     H1 <- NULL
     fr <- func
     # e.g. clogit
-    
+
+    initparams <- c(2.5, -0.8)
+    optimOpt <- c(1000, 1.00000000000000e-08, 1, 1)
+
+
     ab <- max(choice) + 1
     # no interactions in create_logit_input - interact distances in likelihood
         # function instead
     dataCompile <- create_logit_input(choice)
-    
+
     d <- shift_sort_x(dataCompile, choice, catch, distance, max(choice), ab)
-    
+
     starts2 <- initparams
-    
+
     LL_start <- fr(starts2, d, otherdat, max(choice))
-    
+
     if (is.null(LL_start) || is.nan(LL_start) || is.infinite(LL_start)) {
-    
+
         errorExplain <- "Initial function results bad (Nan, Inf, or undefined),
             check 'ldglobalcheck'"
         return("Initial function results bad (Nan, Inf, or undefined),
             check 'ldglobalcheck'")
             # haven't checked what happens when error yet
-    
+
     }
-    
+
     mIter <- optimOpt[1]
     # should add something to default options here if not specified
     relTolX <- optimOpt[2]
     reportfreq <- optimOpt[3]
     detailreport <- optimOpt[4]
-    
+
     controlin <- list(trace = detailreport, maxit = mIter, reltol = relTolX,
         REPORT = reportfreq)
-    
+
     res <- tryCatch({
         optim(starts2, fr, dat = d, otherdat = otherdat, alts = max(choice),
             method = methodname, control = controlin, hessian = TRUE)
     }, error = function(e) {
         return("Optimization error, check 'ldglobalcheck'")
     })
-    
+
     if (res[[1]][1] == "Optimization error, check 'ldglobalcheck'") {
         return(list(errorExplain = res, OutLogit = OutLogit,
             optoutput = optoutput, seoutmat2 = seoutmat2, MCM = MCM, H1 = H1))
     }
-    
+
     q2 <- res[["par"]]
     LL <- res[["value"]]
     output <- list(counts = res[["counts"]], convergence = res[["convergence"]],
         optim_message = res[["message"]])
     H <- res[["hessian"]]
-    
+
     LL <- -LL
-    
+
     # Model comparison metrics (MCM)
     param <- max(dim(as.matrix(starts2)))
     obs <- dim(dataCompile)[1]
     AIC <- (2 * param) - (2 * LL)
-    
+
     AICc <- AIC + ((2 * param * (param + 1))/(obs - param - 1))
-    
+
     BIC <- (-2 * LL) + (param * log(obs))
-    
+
     PseudoR2 <- ((-LL_start) - LL)/(-LL_start)
-    
+
     MCM <- list(AIC = AIC, AICc = AICc, BIC = BIC, PseudoR2 = PseudoR2, LL = LL)
-    
+
     if (is.null(H) == FALSE) {
-    
+
         H1 <- tryCatch({
             solve(H)
         }, error = function(e) {
             return("Error, singular, check 'ldglobalcheck'")
         })
-        
+
         diag2 <- tryCatch({
             diag(H1)
         }, warning = function(w) {
@@ -111,13 +115,13 @@ discretefish_subroutine <- function(catch, choice, distance, otherdat,
         }, error = function(e) {
             return("Error, NAs, check 'ldglobalcheck'")
         })
-        
+
         outmat2 <- tryCatch({
             t(q2)
         }, error = function(e) {
             return("Error, NAs, check 'ldglobalcheck'")
         })
-        
+
         se2 <- tryCatch({
             sqrt(diag2)
         }, warning = function(w) {
@@ -125,21 +129,21 @@ discretefish_subroutine <- function(catch, choice, distance, otherdat,
         }, error = function(e) {
             return(matrix("Error, NAs, check 'ldglobalcheck'", dim(outmat2)[2]))
         })
-        
+
         seoutmat2 <- t(se2)
         optoutput <- output
-        
+
         tLogit <- tryCatch({
             t(outmat2/se2)
         }, error = function(e) {
             return("Error, NAs, check 'ldglobalcheck'")
         })
-        
+
         OutLogit <- cbind(t(outmat2), as.matrix(se2), (tLogit))
-        
+
     }
-    
+
     return(list(errorExplain = errorExplain, OutLogit = OutLogit,
         optoutput = optoutput, seoutmat2 = seoutmat2, MCM = MCM, H1 = H1))
-    
+
 }
